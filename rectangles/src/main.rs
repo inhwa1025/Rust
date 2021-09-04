36ccab09c03f4ccbc2b9253b37904ea6600d75d6
inhwa1025 <inhwa9901@gmail.com>
2021-09-05 03:41:29 +0900

    chapter5_refactoring

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
-    let length1 = 50;
-    let width1 = 30;
+    // let length1 = 50;
+    // let width1 = 30;
+    let rect1 = (50, 30);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(length1, width1)
+        area(rect1)
     );
 }
 
-fn area(length: u32, width: u32) -> u32 {
-    length * width
+fn area(dimentions: (u32, u32)) -> u32 {
+    dimentions.0 * dimentions.1
 }
