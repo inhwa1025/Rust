9e5a610ae53111934ab5cc634c7aaa8680f434e7
inhwa1025 <inhwa9901@gmail.com>
2021-09-04 15:50:01 +0900

    chapter04

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,15 +1,19 @@
 fn main() {
-    
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("the first word is: {}", word);
 }
 
-fn first_word(S: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
---
+++ project/target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\조인화\rust\project\target\debug\project.exe: C:\Users\조인화\rust\project\src\main.rs
