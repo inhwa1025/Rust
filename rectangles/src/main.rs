0abbeef6296c9616da4877bf5bbfc5b7f2554112
inhwa1025 <inhwa9901@gmail.com>
2021-11-07 01:09:17 +0900

    rectangle

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -26,4 +26,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let a = "hello";
+    println!("{}", a.repeat(1));
 }
