b06a6db42cbcc55e35741412b2e4feb6905337c9
inhwa1025 <inhwa9901@gmail.com>
2021-11-19 15:43:55 +0900

    HashMap

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -30,4 +30,13 @@ fn main() {
     for b in "Здравствуйте".bytes() {
         println!("{}", b);
     }
+
+    //Hash map
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("yellow"), 50);
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 }
